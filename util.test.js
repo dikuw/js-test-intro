327294da98c08508e9f64449d6b6b45f516231e3
Mike Williamson <michael@dikuw.com>
2021-06-05 16:03:43 -0400

    added integration tests

--- util.test.js
+++ util.test.js
@@ -1,4 +1,4 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
 test('output name and age', () => {
   const text = generateText('Ashley', 19);
@@ -14,3 +14,8 @@ test('handle null', () => {
   const text = generateText('Ashley', null);
   expect(text).toBe('Ashley (null years old)');
 });
+
+test('generate a valid text output', () => {
+  const text = checkAndGenerate('Sara', 15);
+  expect(text).toBe('Sara (15 years old)');
+})
